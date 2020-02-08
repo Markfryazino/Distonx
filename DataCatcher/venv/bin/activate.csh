b224a9954e941ffe95fc932a7a9bcd91f0bc3dfd
hinser <dima.poljakow@hotmail.com>
2020-02-09 00:14:40 +0300

    Сделал виртуальную среду, куда закинул pymysql; создал папке DataCatcher файл database_saver.py, который надо заимпортить в нужный файл. (оно работает)

--- DataCatcher/venv/bin/activate.csh
+++ DataCatcher/venv/bin/activate.csh
@@ -8,7 +8,7 @@ alias deactivate 'test $?_OLD_VIRTUAL_PATH != 0 && setenv PATH "$_OLD_VIRTUAL_PA
 # Unset irrelevant variables.
 deactivate nondestructive
 
-setenv VIRTUAL_ENV "/home/data_sapiens/programs/projects/Distonx/DataCatcher/venv"
+setenv VIRTUAL_ENV "Distonx/DataCatcher/venv"
 
 set _OLD_VIRTUAL_PATH="$PATH"
 setenv PATH "$VIRTUAL_ENV/bin:$PATH"
---
+++ venv/Scripts/client-script.py
@@ -0,0 +1,12 @@
+#!C:\Users\dimap\PycharmProjects\Distonx\venv\Scripts\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'client==0.0.1','console_scripts','client'
+__requires__ = 'client==0.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('client==0.0.1', 'console_scripts', 'client')()
+    )
