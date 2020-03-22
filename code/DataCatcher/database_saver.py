1a7e757afc18684ced4099ac31f01f73ab768b2c
hinser <dima.poljakow@hotmail.com>
2020-03-22 18:04:58 +0300

    getting data from database by id

--- code/DataCatcher/database_saver.py
+++ code/DataCatcher/database_saver.py
@@ -45,10 +45,16 @@ class db:
         ans = self.cursor.fetchall()
         return ans
 
+    def get_data_by_id(self, id): #возвращает строку по id
+        request = 'SELECT * FROM  `distonx` WHERE id = ' + str(id)
+        self.execute(request)
+        ans = self.cursor.fetchone()
+        return ans
+
     def get_columns_names(
             self):  # Возвращает названия всех стлобцов в том порядке, в котором они находятся в БД
         request = 'SHOW columns FROM distonx;'
         self.execute(request)
         ans = list([i[0] for i in self.cursor.fetchall()])
         return ans
-    
+
