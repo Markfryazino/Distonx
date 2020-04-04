5bf7b4050c825e4a26ecc7fb70c4aaab9f6d79ce
Aleksandr <a@bogdanov.co>
2020-04-04 23:56:55 +0300

    Clean repository and add Docker functionality

---
+++ stonks/web/js/logs.js
@@ -0,0 +1,20 @@
+let tb = document.getElementById('time_balance');
+
+var d;
+
+async function test() {
+    const resp = await fetch('http://0.0.0.0:42069/api/deals?time=123');
+    if (!resp.ok) {
+        alert('we fucked up');
+        return 0
+    }
+    else {
+        const data = resp.json();
+        return data
+    }
+}
+
+test().then(data => {
+    console.log(data);
+    Plotly.newPlot(tb, [{ x: data.data.candle_time_start, y: data.data.amount, type: 'scatter' }]);
+}).catch(e => console.log(e));
