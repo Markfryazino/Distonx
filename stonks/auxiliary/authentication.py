20c0b0b75116ad29bbda5bd1a4d8ae4633ac0e3e
Markfryazino <Rofinmp@gmail.com>
2020-04-07 18:50:28 +0300

    circular bug fixed

--- stonks/auxiliary/authentication.py
+++ stonks/auxiliary/authentication.py
@@ -1,6 +1,5 @@
 from IPython import get_ipython
 from IPython.display import clear_output
-from stonks.DataCatcher import DB
 
 
 def is_notebook():
@@ -23,20 +22,3 @@ def auth_api(level='READ'):
             clear_output()
 
         return token, secret
-
-
-# Запрос пароля для базы данных
-def auth_db():
-    if DB.authorized:
-        return DB.credentials
-    else:
-        address = input('Enter server address: ')
-        login = input('Enter login: ')
-        password = input('Enter password: ')
-        DB.authorized = True
-        DB.credentials = (address, login, password)
-
-    if is_notebook():
-        clear_output()
-
-    return address, login, password
